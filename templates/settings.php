f8c4f696b08d2f42d01ccb2c44d98ed4cc21d73b
B3none <ablackham2000@gmail.com>
2019-12-10 17:16:43 +0000

    Ensure the settings page loads the value if one is applicable

--- templates/settings.php
+++ templates/settings.php
@@ -14,7 +14,7 @@
     <form action="options-general.php?page=<?= $pluginName ?>" method="post">
         <p>
             <label for="<?= $pluginName ?>_website_id"><strong>ResponseTap Website ID</strong></label>
-            <input name="<?= $pluginName ?>_website_id" id="<?= $pluginName ?>_website_id" type="text" />
+            <input name="<?= $pluginName ?>_website_id" id="<?= $pluginName ?>_website_id" type="text" value="<?= htmlentities(get_option('responsetappr_website_id')) ?>" />
         </p>
         <?php wp_nonce_field($pluginName, $pluginName . '_nonce'); ?>
         <p>
