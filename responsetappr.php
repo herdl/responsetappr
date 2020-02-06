dc729b49a8ef24b44d9e196aee0041cc239b4d50
B3none <ablackham2000@gmail.com>
2020-02-06 12:02:48 +0000

    Bump version to 1.1.3

--- responsetappr.php
+++ responsetappr.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/herdl/responsetappr
  * Description: A ResponseTap Wordpress integration.
  * Author: Herdl
- * Version: 1.1.2
+ * Version: 1.1.3
  * Author URI: https://herdl.com
  */
 
